d85cf0acf24cd9c442cfea3e675a1bb11f8f6a3d
manohar <kotamanohar2930@gmail.com>
2024-12-01 12:45:29 +0530

    Project Done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import React from "react";
 import Image from "next/image";
 import { auth, signOut, signIn } from "@/auth";
+import { BadgePlus, LogOut } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 const Navbar = async () => {
   const session = await auth();
   return (
@@ -18,17 +20,29 @@ const Navbar = async () => {
         <div className="flex items-center gap-5 text-black">
           {session && session?.user ? (
             <>
-              <Link href="/startup/create">create</Link>
+              <Link href="/startup/create">
+                <span className="max-sm:hidden">Create</span>
+                <BadgePlus className="size-6 sm:hidden text-red-500" />
+              </Link>
               <form
                 action={async () => {
                   "use server";
                   await signOut({ redirectTo: "/" });
                 }}
               >
-                <button type="submit">SignOut</button>
+                <button type="submit">
+                  <span className="max-sm:hidden">Logout</span>
+                  <LogOut className="size-6 sm:hidden text-red-500" />
+                </button>
               </form>
               <Link href={`user/${session?.id}`}>
-                <span>{session?.user?.name}</span>
+                <Avatar className="size-10">
+                  <AvatarImage
+                    src={session?.user?.image}
+                    alt={session?.user?.name || " "}
+                  />
+                  <AvatarFallback>AV</AvatarFallback>
+                </Avatar>
               </Link>
             </>
           ) : (
--- components/View.tsx
+++ components/View.tsx
@@ -1,16 +1,14 @@
-import React from "react";
-import Ping from "./Ping";
+import Ping from "@/components/Ping";
 import { client } from "@/sanity/lib/client";
-import { STARTUPS_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 import { writeClient } from "@/sanity/lib/write-client";
 import { unstable_after as after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
-    .fetch(STARTUPS_VIEWS_QUERY, { id });
+    .fetch(STARTUP_VIEWS_QUERY, { id });
 
-  //to update views
   after(
     async () =>
       await writeClient
@@ -24,11 +22,11 @@ const View = async ({ id }: { id: string }) => {
       <div className="absolute -top-2 -right-2">
         <Ping />
       </div>
+
       <p className="view-text">
-        <span className="font-black">views: {totalViews} </span>
+        <span className="font-black">Views: {totalViews}</span>
       </p>
     </div>
   );
 };
-
 export default View;
